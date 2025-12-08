c4ebb89d6ec6bce29a3406f71981367d5d431738
fm <fm>
2025-12-08 16:28:31 +0530

    Fixes #8 and #9

---
+++ C++ Primer/chapter2/exe2_4.cpp
@@ -0,0 +1,43 @@
+/*
+ * Exercise 2.3: What output will the following code produce?\
+ * 	unsigned u = 10, u2 = 42;
+ * 	std::cout << u2 - u << std::endl;
+ * 	std::cout << u - u2 << std::endl;
+ * 	int i = 10, i2 = 42;
+ * 	std::cout << i2 - i << std::endl;
+ * 	std::cout << i - i2 << std::endl;
+ * 	std::cout << i - u << std::endl;
+ * 	std::cout << u - i << std::endl;
+ */
+
+#include <iostream>
+int main(){
+
+	unsigned u = 10, u2 = 42;
+	std::cout << u2 - u << std::endl;
+	std::cout << u - u2 << std::endl;
+	int i = 10, i2 = 42;
+	std::cout << i2 - i << std::endl;
+	std::cout << i - i2 << std::endl;
+	std::cout << i - u << std::endl;
+	std::cout << u - i << std::endl;
+
+	return 0;
+
+}
+// predictions:
+// 32
+// 2147483646
+// 30
+// -32
+// 0
+// 0
+//
+// output:
+// 32
+// 4294967264
+// 32
+// -32
+// 0
+// 0
+
