dc533959767ed7767d1dcb8948c679022f197a5e
fm <fm>
2025-12-09 11:24:27 +0530

    Fixes #9

--- C++ Primer/chapter2/exe2_4.cpp
+++ C++ Primer/chapter2/exe2_4.cpp
@@ -1,13 +1,6 @@
 /*
- * Exercise 2.3: What output will the following code produce?\
- * 	unsigned u = 10, u2 = 42;
- * 	std::cout << u2 - u << std::endl;
- * 	std::cout << u - u2 << std::endl;
- * 	int i = 10, i2 = 42;
- * 	std::cout << i2 - i << std::endl;
- * 	std::cout << i - i2 << std::endl;
- * 	std::cout << i - u << std::endl;
- * 	std::cout << u - i << std::endl;
+ * Exercise 2.4: Write a program to check whether your predictions were correct. If not, 
+ * study this section until you understand what the problem is.
  */
 
 #include <iostream>
